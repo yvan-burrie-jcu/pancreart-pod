a43c4b8e91d58c7542a84dbb6540f576548abfae
Jesse <jesse.kerridgebyrne@my.jcu.edu.ua>
2020-09-20 15:52:48 +1000

    Add insulin reservoir unit tests, and file comments. Add insulin reservoir attributes and methods.

--- test/TestInsulinReservoir.java
+++ test/TestInsulinReservoir.java
@@ -1,2 +1,20 @@
-public class TestInsulinReservoir {
+import models.InsulinReservoir;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestInsulinReservoir extends InsulinReservoir {
+    @Test
+    void testDefaults() {
+        // Test that insulin is present
+        assertTrue(InsulinReservoir.isInsulinPresent());
+
+        // Test that insulin can be removed
+        InsulinReservoir.removeInsulin();
+        assertFalse(InsulinReservoir.isInsulinPresent());
+
+        // Test that insulin can be replaced
+        InsulinReservoir.replaceInsulin();
+        assertTrue(InsulinReservoir.isInsulinPresent());
+    }
 }
--- test/TestPowerSupply.java
+++ test/TestPowerSupply.java
@@ -1,2 +1,6 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestPowerSupply {
 }
