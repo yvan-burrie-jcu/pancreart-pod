b766a6fb65c391fffbdd708d352bf83fbf6582a6
Jesse <jesse.kerridgebyrne@my.jcu.edu.ua>
2020-09-20 15:07:07 +1000

    Add insulin pump unit tests, and file comments. Add insulin pump attributes and methods.

--- test/TestInsulinPump.java
+++ test/TestInsulinPump.java
@@ -1,2 +1,26 @@
-public class TestInsulinPump {
+import models.InsulinPump;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestInsulinPump extends InsulinPump {
+    @Test
+    void testDefaults() {
+        // Initial pump state test
+        assertTrue(InsulinPump.isPumpWorking());
+
+        // Test pump state change
+        InsulinPump.setPumpWorking(false);
+        assertFalse(InsulinPump.isPumpWorking());
+        InsulinPump.setPumpWorking(true);
+        assertTrue(InsulinPump.isPumpWorking());
+
+        // Test insulin delivery result
+        assertTrue(InsulinPump.deliverInsulin(10));
+
+        // Test pump state change and how delivery is affected
+        InsulinPump.setPumpWorking(false);
+        assertFalse(InsulinPump.isPumpWorking());
+        assertFalse(InsulinPump.deliverInsulin(10));
+    }
 }
