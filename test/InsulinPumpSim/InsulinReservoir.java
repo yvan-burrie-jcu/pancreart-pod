562390fedb322dc7b6e2c210ffdb9493bdec6b83
Jesse <jesse.kerridgebyrne@my.jcu.edu.ua>
2020-09-20 13:14:12 +1000

    Add refactored sample Insulin Pump Sim to test directory.

---
+++ test/InsulinPumpSim/InsulinReservoir.java
@@ -0,0 +1,22 @@
+package InsulinPumpSim;
+
+class InsulinReservoir
+{
+	private static boolean insulinPresent = true;
+
+	public static void removeInsulin()
+	{
+		insulinPresent = false;
+	}
+
+	public static void replaceInsulin()
+	{
+		insulinPresent = true;
+		Controller.insulinReplaced();
+	}
+
+	public static boolean isInsulinPresent()
+	{
+		return insulinPresent;
+	}
+}
