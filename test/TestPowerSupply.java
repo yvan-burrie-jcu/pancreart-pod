76706219d7257b7d8c8f2663021287d8dfbc4bfc
Jesse <jesse.kerridgebyrne@my.jcu.edu.ua>
2020-09-20 16:25:02 +1000

    Add power supply unit tests, and file comments. Add power supply attributes and methods.

--- test/TestPowerSupply.java
+++ test/TestPowerSupply.java
@@ -1,6 +1,20 @@
+import models.PowerSupply;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestPowerSupply {
+public class TestPowerSupply extends PowerSupply {
+    @Test
+    void testDefaults() {
+        // Test initial battery level
+        assertEquals(Level.HIGH, PowerSupply.batteryLevel());
+
+        // Test battery level change through all values
+        PowerSupply.setBatteryLevel();
+        assertEquals(Level.MEDIUM, PowerSupply.batteryLevel());
+        PowerSupply.setBatteryLevel();
+        assertEquals(Level.LOW, PowerSupply.batteryLevel());
+        PowerSupply.setBatteryLevel();
+        assertEquals(Level.HIGH, PowerSupply.batteryLevel());
+    }
 }
