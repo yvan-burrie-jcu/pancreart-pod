4741b611f14685b4ed7320723aa2960d23f35662
Jesse <jesse.kerridgebyrne@my.jcu.edu.ua>
2020-10-27 12:01:41 +1000

    Modify Sensor model to set a random initial reading for glucose.
    Signed-off-by: Jesse <jesse.kerridgebyrne@my.jcu.edu.ua>

--- src/main/java/jcu/cp3407/pancreart/model/Sensor.java
+++ src/main/java/jcu/cp3407/pancreart/model/Sensor.java
@@ -1,9 +1,17 @@
 package jcu.cp3407.pancreart.model;
 
+import java.util.Random;
+
 public class Sensor {
+    // TODO: 27/10/2020 add test methods for Sensor class
     private static int currentReading = 0;
+    private static final Random randomReading = new Random();
 
     Sensor() {
+    }
+
+    public static void setReading(int start, int end) {
+        currentReading = randomReading.nextInt(12 + 15);
     }
 
     public static void setReading(int newReading) {
