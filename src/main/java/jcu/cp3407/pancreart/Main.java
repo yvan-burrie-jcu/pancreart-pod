271d62d660a47edb7fdfc007a231117613a43166
Jesse <jesse.kerridgebyrne@my.jcu.edu.ua>
2020-10-27 12:32:10 +1000

    NOTE: Work In Progress Add temporary Main class to activate the simulation.
    Signed-off-by: Jesse <jesse.kerridgebyrne@my.jcu.edu.ua>

---
+++ src/main/java/jcu/cp3407/pancreart/Main.java
@@ -0,0 +1,10 @@
+package jcu.cp3407.pancreart;
+
+import jcu.cp3407.pancreart.controller.Simulator;
+
+public class Main {
+    public static void main(String[] args) {
+        Simulator simulator = new Simulator();
+        simulator.run();
+    }
+}
